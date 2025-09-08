aadc7f057af57e99ed4a760190d0d29df594220e
p0m3n <pomendhdls@gmail.com>
2025-09-08 13:45:07 -0600

    First Push
    Prueba para que los demás vean el cambio

--- Assets/Scripts/Github_Testing.cs
+++ Assets/Scripts/Github_Testing.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Ejemplo1 : MonoBehaviour
+public class Github_Testing : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
