216c38cf3518e335a1b769c2a36181b9fcc8dc65
OneSaucyBoy <miguelbautistach@gmail.com>
2025-09-09 15:51:51 -0600

    Enemies spawner corregido

--- Assets/Scripts/Enemy scripts/Enemy_Spawner.cs
+++ Assets/Scripts/Enemy scripts/Enemy_Spawner.cs
@@ -5,7 +5,7 @@ public class Enemy_Spawner : MonoBehaviour
 {
     public float timer = 10f;
     private int x, y;
-    private Transform spawnPosition;
+    private Vector3 spawnPosition =Vector3.zero;
     public GameObject enemyPrefab_Mike;
     public GameObject enemyPrefab_Casta;
     public GameObject enemyPrefab_Espi;
@@ -27,51 +27,54 @@ public class Enemy_Spawner : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
-        if (timer <= 0f)
+
+        if (timer < 0f)
         {
             x = Random.Range(1, 6);
             y = Random.Range(1, 5);// numeros random para elegir que enemigo y en que carril
             switch (y)
             {
                 case 1:
-                    spawnPosition.position = spawnPoint1.position;
+                    spawnPosition = spawnPoint1.position;
                     break;
                 case 2:
-                    spawnPosition.position = spawnPoint2.position;
+                    spawnPosition = spawnPoint2.position;
                     break;
                 case 3:
-                    spawnPosition.position = spawnPoint3.position;
+                    spawnPosition = spawnPoint3.position;
                     break;
                 case 4:
-                    spawnPosition.position = spawnPoint4.position;
+                    spawnPosition = spawnPoint4.position;
                     break;
                 case 5:
-                    spawnPosition.position = spawnPoint5.position;
+                    spawnPosition = spawnPoint5.position;
                     break;
                 case 6:
-                    spawnPosition.position = spawnPoint6.position;
+                    spawnPosition = spawnPoint6.position;
                     break;
             }
             switch (x)
             {
                 case 1:
-                    Instantiate(enemyPrefab_Mike, spawnPosition);
+                    Instantiate(enemyPrefab_Mike, spawnPosition, Quaternion.identity);
                     break;
                 case 2:
-                    Instantiate(enemyPrefab_Casta, spawnPosition);
+                    Instantiate(enemyPrefab_Casta, spawnPosition, Quaternion.identity);
                     break;
                 case 3:
-                    Instantiate(enemyPrefab_Espi, spawnPosition);
+                    Instantiate(enemyPrefab_Espi, spawnPosition, Quaternion.identity);
                     break;
                 case 4:
-                    Instantiate(enemyPrefab_Santi, spawnPosition);
+                    Instantiate(enemyPrefab_Santi, spawnPosition, Quaternion.identity);
                     break;
                 case 5:
-                    Instantiate(enemyPrefab_Doc, spawnPosition);
+                    Instantiate(enemyPrefab_Doc, spawnPosition, Quaternion.identity);
                     break;
             }
             timer = 10f;// reseteo del timer
         }
+       
     }
+
     
 }
